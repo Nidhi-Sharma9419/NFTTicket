a6ec4fa72480a29db67b284bc5af2e64061cf0b9
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-06-01 00:47:59 +0530

    Updated

--- src/components/TrendingNfts/index.js
+++ src/components/TrendingNfts/index.js
@@ -2,7 +2,12 @@ import React from 'react'
 import './trendingNfts.css';
 const TrendingNfts = () => {
   return (
-    <div>TrendingNfts</div>
+    <div className='trending-nfts'>
+      <div className='tn-title absolute-center'>
+      <span className='heading-gradient'>TRENDING NFTs</span>
+      <div className='tn-bg-blob'></div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/data/trendingNfts.js
@@ -0,0 +1,6 @@
+export const TRENDING_NFTS = [
+    {
+        id: 1,
+        
+    }
+]
