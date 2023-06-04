4958c8b1611cb175dc0c6e9dee4300999f36d400
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-06-04 21:14:19 +0530

    Update header.jsx

--- src/MyComponents/header.jsx
+++ src/MyComponents/header.jsx
@@ -30,10 +30,10 @@ export default function Header() {
 
 
                         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]
-                 border-b-transparent ${pathMatchRoute("/events") && "text-black border-b-red-500"}`}
+                border-b-transparent ${pathMatchRoute("/events") && "text-black border-b-red-500"}`}
                             onClick={() => navigate("/events")}>Events</li>
                         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]
-                 border-b-transparent ${pathMatchRoute("/book-tickets") && "text-black border-b-red-500"}`}
+                border-b-transparent ${pathMatchRoute("/book-tickets") && "text-black border-b-red-500"}`}
                             onClick={() => navigate("book-tickets")}>BookTickets</li>
                     </ul>
                 </div>
