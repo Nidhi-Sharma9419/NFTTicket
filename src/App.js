ea329cf6f8c9809ec1527572214a551c73d663f1
sarmad505 <sarmadbaig505@gmail.com>
2023-06-06 04:53:18 +0500

    Add create event form , create ticket form update header

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './MyComponents/header';
 import Home from "./pages/Home"
 import Events from "./pages/Events"
 import BookTickets from "./pages/BookTickets";
+import Resale from "./pages/Resale";
 
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
       <Route path="/" element={<Home />}></Route>
           <Route path="/events" element={<Events />}></Route>
           <Route path="/book-tickets" element={<BookTickets />}></Route>
+          <Route path="/resale-tickets" element={<Resale />}></Route>
         
       </Routes>
     </Router>
