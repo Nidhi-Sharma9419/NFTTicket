c618cd44ce646bc2ffc0db588cbfb917842bdee4
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-06-07 22:08:18 +0530

    Update Connect.jsx

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Web3Modal from 'web3modal';
 import { ethers } from "ethers";
+//import { Web3Provider } from 'ethers';
 import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
 
 
@@ -11,26 +12,29 @@ import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
 //const provider = new ethers.providers.JsonRpcProvider('https://example-rpc-provider.com');
 //import { EthereumProvider } from '@walletconnect/ethereum-provider'
 
-const providerOptions = {
-    coinbasewallet:{
-        package: CoinbaseWalletSDK,
-        options:{
-            appName: 'Web3Modal Demo',
-            infuraId: {1: 'https://mainnet.infura.io/v3/9aa8dd8ca6ea43058bfd66e6980ef75b'}
-        }
-    }
-}
+
 
 export default function Connect (){
+    
+    const providerOptions = {
+        coinbasewallet:{
+            package: CoinbaseWalletSDK,
+            options:{
+                appName: 'Web3Modal Demo',
+                infuraId: {1: 'https://mainnet.infura.io/v3/9aa8dd8ca6ea43058bfd66e6980ef75b'}
+            }
+        }
+    }    
 
 async function connectWallet(){
+   
     try {
         let web3Modal = new Web3Modal( {
         cacheProvider: false,
         providerOptions,
     });
     const web3ModalInstance = await web3Modal.connect();
-    const web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance);
+    const web3ModalProvider = new ethers.BrowserProvider(web3ModalInstance);
     console.log(web3ModalProvider);
     } catch (error) { 
 
