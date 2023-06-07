c90038240c075dc0fb68d9becc50c9355da7e602
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-06-07 21:00:23 +0530

    Connect button

--- src/MyComponents/header.jsx
+++ src/MyComponents/header.jsx
@@ -1,16 +1,16 @@
 
 import image from '../logo.svg'
-import wallet from '../assets/metamask-logo.png'
+//import wallet from '../assets/metamask-logo.png'
 
 import { useLocation, useNavigate } from "react-router-dom";
 export default function Header() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  function pathMathRoute(route) {
+const location = useLocation();
+const navigate = useNavigate();
+function pathMatchRoute(route) {
     if (route === location.pathname) {
-      return true;
+        return true;
     }
-  }
+}
     return (
         <div className='bg-zinc-900 border-b shadow-sm sticky top-0 z-50'>
             <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
@@ -26,7 +26,7 @@ export default function Header() {
 
 
 
-            <ul className='flex space-x-5'>
+                <ul className='flex space-x-5'>
                         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"
                             }`} onClick={() => navigate("/")}
                         >Home</li>
@@ -42,7 +42,7 @@ export default function Header() {
                 border-b-transparent ${pathMatchRoute("/book-tickets") && "text-black border-b-red-500"}`}
                             onClick={() => navigate("/book-tickets")}>BookTickets</li>
                         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]
-                border-b-transparent ${pathMathRoute("/book-tickets") && "text-black border-b-red-500"}`}
+                border-b-transparent ${pathMatchRoute("/book-tickets") && "text-black border-b-red-500"}`}
                             onClick={() => navigate("resale-tickets")}>Resale</li>
                     </ul>
                 </div> 
--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -1,44 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 import Web3Modal from 'web3modal';
 import { ethers } from "ethers";
+import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
+
+
 
 
 //const ethersProviders = ethers.providers;
+//const provider = new ethers.providers.JsonRpcProvider('https://example-rpc-provider.com');
+// import { EthereumProvider } from '@walletconnect/ethereum-provider'
 
-const providerOptions = {
+// const providerOptions = {
+//     coinbasewallet:{
+//         package: CoinbaseWalletSDK,
+//         options:{
+//             appName: 'Web3Modal Demo',
+//             infuraId: {1: 'https://mainnet.infura.io/v3/9aa8dd8ca6ea43058bfd66e6980ef75b'}
+//         }
+//     }
+// }
 
-}
+// export default function Connect (){
 
-export default function Connect (){
+// async function connectWallet(){
+//     try {
+//         let web3Modal = new Web3Modal( {
+//         cacheProvider: false,
+//         providerOptions,
+//     });
+//     const web3ModalInstance = await web3Modal.connect();
+//     const web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance);
+//     console.log(web3ModalProvider);
+//     } catch (error) { 
 
-async function connectWallet(){
-    try {
-        let web3Modal = new Web3Modal( {
-        cacheProvider: false,
-        providerOptions,
-    });
-    const web3ModalInstance = await web3Modal.connect();
-    const web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance);
-    console.log(web3ModalProvider);
-    } catch (error) { 
+//     console.error(error);
 
-    console.error(error);
+//     }
+// }
 
-    }
-}
-
-return (
+// return (
 
     
-    <div className='App-header'>
-        <h1>Web3Modal Connection!</h1>
-                <button onClick={connectWallet}>
-                    Connect Wallet
-                </button>
-    </div>
+//     <div className='App-header'>
+//         <h1>Web3Modal Connection!</h1>
+//                 <button onClick={connectWallet}>
+//                     Connect Wallet
+//                 </button>
+//     </div>
     
-)
-}
+// )
+// }
 
 //export default Connect;
+
+import { EthereumProvider } from '@walletconnect/ethereum-provider'
+
+export default function Connect() {
+  const [provider, setProvider] = useState(undefined)
+
+  async function onInitializeProviderClient() {
+    const client = await EthereumProvider.init({
+      projectId: 'YOUR_PROJECT_ID',
+      showQrModal: true,
+      qrModalOptions: { themeMode: 'light' },
+      chains: [1],
+      methods: ['eth_sendTransaction', 'personal_sign'],
+      events: ['chainChanged', 'accountsChanged'],
+      metadata: {
+        name: 'My Dapp',
+        description: 'My Dapp description',
+        url: 'https://my-dapp.com',
+        icons: ['https://my-dapp.com/logo.png']
+      }
+    })
+
+    setProvider(client)
+  }
+
+  useEffect(() => {
+    onInitializeProviderClient()
+  }, [])
+
+  return provider ? (
+    <button onClick={() => provider.connect()}>Connect Wallet</button>
+  ) : (
+    'Loading...'
+  )
+}
